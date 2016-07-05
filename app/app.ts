f06e36d5d891a05d6503df9d21aa6b1de65bfeed
yiochen <chen.yiou1992@gmail.com>
2016-07-05 17:24:24 -0400

    settings page

--- app/app.ts
+++ app/app.ts
@@ -5,6 +5,9 @@ import {TabsPage} from './pages/tabs/tabs';
 import {Xml} from './providers/xml/xml';
 import {DeviService} from './providers/devi-service/devi-service';
 import {BookmarkService} from './providers/bookmark-service/bookmark-service';
+
+import {SettingsPage} from './pages/settings/settings';
+
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>',
   providers:[Xml, DeviService, BookmarkService]
@@ -15,8 +18,8 @@ export class MyApp {
 
   constructor(private platform:Platform,
   private bookmarkService:BookmarkService) {
-    this.rootPage = TabsPage;
-
+    //this.rootPage = TabsPage;
+    this.rootPage = SettingsPage;
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
